efb39b7130b35643fcaffc6dc89e9337750354b8
Tassot Arthur <arthurtassot@gmail.com>
2022-05-16 16:12:33 +0200

    partiel

--- api/index.js
+++ api/index.js
@@ -1,10 +1,38 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const fs = require('fs');
 const app = express();
-const port = 3001;
+const port = 3003;
 
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+
+var data = fs.readFileSync('data.json');
+var myObject= JSON.parse(data);
+
+
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('connected', socket.client.id);
+    console.log( socket.client.conn.server.clientsCount + " users connected" );
+    getUsersConnected(socket);
+})
+
+const getUsersConnected = socket => {
+    const response = socket.client.conn.server.clientsCount;
+    socket.emit("FromAPI", response);
+};
 let tickets = [];
 
 app.use(cors());
@@ -14,6 +42,17 @@ app.use(bodyParser.json());
 
 app.post('/buy', (req, res) => {
 
+    let newData = [12,32,14];
+    myObject.push(newData);
+    var newData2 = JSON.stringify(myObject);
+    fs.writeFile('data.json', newData2, err => {
+        // error checking
+        if(err) throw err;
+    
+    console.log("New data added");
+});   
 });
 
-app.listen(port, () => console.log(`CHAMBERY BILLETTERIE ${port}!`));
+server.listen(port, function() {
+    console.log('listening on localhost:' + port);
+});
--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
 import {useEffect, useState} from "react";
+import {io} from "socket.io-client";
 
 function App() {
     const [places, setPlaces] = useState();
+    const [selectedPlaces , setSelectedPlaces] = useState([]);
+    const [usersConnected, setUsersConnected] = useState("");
+
     const placesCount = 50;
+
+
+    useEffect(() => {
+        const socket = io("http://localhost:3003", {});
+
+        socket.on("FromAPI", data => {
+            setUsersConnected(data);
+          });
+        
+        return () => {
+            socket.disconnect();
+            console.log('socket disconnected');  
+        }
+    }, [])
+   
+
+    function addPlace (i) {
+
+        const copy = [... selectedPlaces];
+     
+        if(copy.includes(i)) {
+            copy.splice(copy.indexOf(i) , 1)
+        }else {
+            copy.push(i);
+        }
+    
+        if(copy.length > 3) {
+            copy.shift();
+            
+        }
+        setSelectedPlaces(copy)
+
+    }
+
+    // const stockPlaces = () => {
+    //     fetch('http://localhost:3001/buy' , {
+    //         headers: {'Content-Type' : 'application/json'},
+    //         method : 'POST',
+    //         body : JSON.stringify({
+               
+    //         })
+    //     })
+    // }
 
     useEffect(() => {
         const a = [];
@@ -11,11 +58,17 @@ function App() {
             const y = Math.floor(i / (step)) * 42;
             const x = i % step * 42;
             a.push(<button key={i}
-                         style={{left: x, top: y}}
-                         className={"absolute hover:bg-yellow-300 rounded-sm overflow-hidden flex items-center justify-center border border-gray-300 w-10 h-10"}>{i}</button>);
+                        style={{left: x, top: y }}
+                        onClick={ () => addPlace(i)}
+                        className={'absolute hover:bg-yellow-300 rounded-sm overflow-hidden flex items-center justify-center border border-gray-300 w-10 h-10 '}
+                
+                        >{i}</button>);
         }
+        console.log(selectedPlaces)
         setPlaces(a);
-    }, [])
+
+
+    }, [selectedPlaces])
 
     return (
         <div className="h-screen flex items-center justify-center w-full">
@@ -24,6 +77,8 @@ function App() {
             <div className={"flex justify-end flex-col"}>
                 <div className={"w-[420px] h-[210px] relative"}>{places}</div>
                 <button className={"bg-black border-0 mt-1 text-white py-2 hover:bg-yellow-300 hover:text-black"}>Acheter</button>
+                <span>Places selectionnées : {JSON.stringify(selectedPlaces)}</span>
+                <span>Utilisateurs connectés :{usersConnected}</span>
             </div>
         </div>
     );
